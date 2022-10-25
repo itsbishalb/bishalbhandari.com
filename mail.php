f48359e534fd59126079227421ca87d507e7a45e
psybb7 <psybb7@nottingham.ac.uk>
2022-10-25 22:01:18 +0100

    test mail

--- mail.php
+++ mail.php
@@ -1,12 +1,13 @@
 <?php 
-if(isset($_POST['submit'])){
-    $name = $_POST('name');
-    $email = $_POST('email');
-    $subject = $_POST('subject');
-    $message = $_POST('message');
+    if(isset($_POST['submit'])){
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $subject = $_POST['subject'];
+        $message = $_POST['message'];
 
-    $mailTo = "hello@bishalbhandari.com"
-    $headers = "From:" . $email;
-    $txt = "You have received an emal from " . $name . ".\n \n" .$message;
-    mail($mailTo,$subject,$txt,$headers)
-}
+        $mailTo = "hello@bishalbhandari.com";
+        $headers = "From:" . $email;
+        $txt = "You have received an emal from " . $name . ".\n \n" .$message;
+        mail($mailTo,$subject,$txt,$headers);
+    }
+?>
